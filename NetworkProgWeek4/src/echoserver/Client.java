d9c7efe2db8e40a3b3ed26e45da8a934ab9bcab9
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-03-23 19:16:15 +1100

    Added comments

--- NetworkProgWeek4/src/echoserver/Client.java
+++ NetworkProgWeek4/src/echoserver/Client.java
@@ -10,23 +10,10 @@ import java.net.UnknownHostException;
 
 
 /**
- * Connected to 127.0.0.1 on port 15376
- * 
- * +----------------------------------+
- * | INSTRUCTIONS:                    |
- * | - Type your input.               |
- * | - Press enter to send            |
- * | - Type 'X' then enter to exit    |
- * +----------------------------------+
- * 
- * Input: Hello
- * Reply: Hello
- * Input: World
- * Reply: World
- * Input: X
- * Reply: X
- * 
- * ******* Connection terminated ********
+ * Simple program that send lines of messages to a socket server<br/><br/>
+ * On startup, you may provide these 2 optional arguments:<br/>
+ * - <b>Host name</b>. Defaults to '127.0.0.1'<br/>
+ * - <b>Port number</b>. Defaults to port 15376<br/>
  */
 public class Client {
 	
@@ -35,6 +22,7 @@ public class Client {
 
 	public static void main(String[] args) {
 		
+		// get port and host from command line args
 		int port = getPortNumber(args);
 		String address = getHostAddress(args);
 		
@@ -44,16 +32,21 @@ public class Client {
 		BufferedReader consoleInput = null;
 		
 		try {
+			
+			// connect to server
 			socket = new Socket(address, port);
 			System.out.println("Connected to " + address + " on port " + port);
 			
-			
+			// setup input and output streams
 			output = new PrintWriter(socket.getOutputStream(), true);
 			input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			consoleInput = new BufferedReader(new InputStreamReader(System.in));
 			
 			printInstructions();
 			
+			// Read line from console. On enter pressed, this client sends the message to the server
+			// This client expects replies from server. 
+			// When this client receives an 'X' character, the program quits
 			String line;
 			System.out.print("Input: ");
 			while ((line = consoleInput.readLine()) != null) {
@@ -71,14 +64,20 @@ public class Client {
 			
 			connectionTerminated();
 			
-		} catch (UnknownHostException unknownHostEx) {
+		} 
+		// Bad/unknown host
+		catch (UnknownHostException unknownHostEx) {
 			System.err.println("ERROR: Unknown host " + address);
 			unknownHostEx.printStackTrace();
 		}
+		// Some errors like network interruption, security problems, etc
 		catch (IOException ioEx) {
 			System.err.println("ERROR: Sorry! Server hasn't started yet or something went wrong");
 			ioEx.printStackTrace();
-		} finally {
+		}
+		// close connection gracefully, 
+		// socket connection opened first, so close it last.
+		finally {
 			try {
 				if (consoleInput != null) consoleInput.close();
 				if (input != null) input.close();
@@ -91,6 +90,14 @@ public class Client {
 		}
 	}
 
+	
+	/**
+	 * Gets port number from command line arguments.<br/>
+	 * Port number must be the second command line argument
+	 * 
+	 * @param args
+	 * @return int
+	 */
 	public static int getPortNumber(String[] args) {
 		int portNumber = DEFAULT_PORT_NUMBER;
 		
@@ -109,6 +116,13 @@ public class Client {
 	}
 	
 	
+	/**
+	 * Gets the host address from command line arguments
+	 * Host address must be the first command line argument
+	 * 
+	 * @param args
+	 * @return
+	 */
 	public static String getHostAddress(String[] args) {
 		String address = DEFAULT_HOST;
 		
@@ -121,6 +135,9 @@ public class Client {
 	}
 	
 	
+	/**
+	 * Simple callback to print instructions
+	 */
 	public static void printInstructions() {
 		System.out.println();
 		System.out.println("+----------------------------------+");
@@ -132,6 +149,11 @@ public class Client {
 		System.out.println();
 	}
 	
+	
+	/**
+	 * Simple callback to show message that connection to 
+	 * server was terminated
+	 */
 	public static void connectionTerminated() {
 		System.out.println();
 		System.out.println("******* Connection terminated ********");
--- NetworkProgWeek4/src/echoserver/Server.java
+++ NetworkProgWeek4/src/echoserver/Server.java
@@ -6,38 +6,25 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.*;
 
+
 /**
- * EchoServer started. 
- * Listening on port 15376...
- * 
- * +-----------------------------+
- * |       Client connected      |
- * +-----------------------------+
- * Client: Hello
- * Client: World
- * Client: X
- * 
- * ******** CLIENT DISCONNECTED *********
- * 
- * Listening on port 15376...
- * 
- * +-----------------------------+
- * |       Client connected      |
- * +-----------------------------+
- * Client: Hello
- * Client: World
- * Client: X
- * 
- * ******** CLIENT DISCONNECTED *********
- * 
+ * Simple server program that takes user input and throws 
+ * it back to the client<br/><br/>
+ * Launches a socket server at port 15376 by default.<br/>
+ * Server will continue to listen unless the user 
+ * stops the server manually or some error happens
  */
 public class Server {
+	
 	
 	public static final int DEFAULT_PORT_NUMBER = 15376;
 	
 	
 	public static void main(String[] args) {
+		
+		// get the port from args. If args is empty, use port 15376
 		int port = getPortNumber(args);
+		
 		ServerSocket serverSocket = null;
 		Socket clientSocket = null;
 		PrintWriter out = null;
@@ -48,6 +35,8 @@ public class Server {
 			serverSocket = new ServerSocket(port);
 			waitingForConnection(port);
 			
+			
+			// listen for connections 24/7
 			do {
 				// accept client
 				clientSocket = serverSocket.accept();
@@ -71,12 +60,17 @@ public class Server {
 			
 			
 			
-		} catch (IllegalArgumentException argEx) {
+		} 
+		// Invalid port number
+		catch (IllegalArgumentException argEx) {
 			System.err.println("Invalid port number");
-		} catch (IOException ioEx) {
+		}
+		// Other exceptions like network interruption, security errors, etc
+		catch (IOException ioEx) {
 			System.err.println("Sorry! Something went wrong.");
 			ioEx.printStackTrace();
-		} finally {
+		} 
+		finally {
 			try {
 				if (input != null) input.close();
 				if (out != null) out.close();
@@ -89,6 +83,12 @@ public class Server {
 	}
 	
 	
+	/**
+	 * Get the port number provided by command line arguments
+	 * 
+	 * @param args
+	 * @return int
+	 */
 	public static int getPortNumber(String[] args) {
 		int portNumber = DEFAULT_PORT_NUMBER;
 		
@@ -107,6 +107,9 @@ public class Server {
 	}
 	
 	
+	/**
+	 * Simple callback saying the client is connected
+	 */
 	public static void clientConnected() {
 		System.out.println();
 		System.out.println("+-----------------------------+");
@@ -116,12 +119,23 @@ public class Server {
 		
 	}
 	
+	
+	/**
+	 * Simple callback saying client is disconnected
+	 */
 	public static void clientDisconnected() {
 		System.out.println();
 		System.out.println("******** CLIENT DISCONNECTED *********");
 		System.out.println();
 	}
 	
+	
+	/**
+	 * Simple callback saying the server is waiting for 
+	 * connection at this port
+	 * 
+	 * @param port
+	 */
 	public static void waitingForConnection(int port) {
 		System.out.println("Listening on port " + port + "...");
 	}
