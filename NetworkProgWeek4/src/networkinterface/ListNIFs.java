840feb666eb8ba40b0dc2409d7bb0434199b34c4
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-03-23 19:23:26 +1100

    Added comments in network interface (task 1)

--- NetworkProgWeek4/src/networkinterface/ListNIFs.java
+++ NetworkProgWeek4/src/networkinterface/ListNIFs.java
@@ -5,22 +5,46 @@ import java.net.*;
 import java.util.*;
 import static java.lang.System.out;
 
+/**
+ * Program that prints<br/>
+ * - name of network interface</br>
+ * - actual name <br/>
+ * - hardware address (MAC)<br/>
+ * - list of sub-interfaces<br/>
+ * - list of host names and host addresses<br/>
+ * 
+ * @author Joshua Orozco
+ */
 public class ListNIFs 
 {
     public static void main(String args[]) throws SocketException {
         Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
         
         for (NetworkInterface netIf : Collections.list(nets)) {
+        	
+        	// display name and actual name
             out.printf("Display name: %s\n", netIf.getDisplayName());
             out.printf("Name: %s\n", netIf.getName());
+            
+            // MAC adddress in hex form, separated by ':'
             displayMACAddress(netIf);
+            
+            // subinterfaces
             displaySubInterfaces(netIf);
+            
+            // all hosts, include each host name and host address
             displayHostsInfo(netIf);
+            
         	out.printf("\n");
         }
     }
 
-    
+    /**
+     * Prints list of sub-interfaces' display and actual names
+     * 
+     * @param netIf
+     * @throws SocketException
+     */
     static void displaySubInterfaces(NetworkInterface netIf) throws SocketException {
         Enumeration<NetworkInterface> subIfs = netIf.getSubInterfaces();
         
@@ -31,6 +55,12 @@ public class ListNIFs
     }
     
     
+    /**
+     * Prints associated hosts' names and addresses
+     * 
+     * @param netIf
+     * @throws SocketException
+     */
     static void displayHostsInfo(NetworkInterface netIf) throws SocketException {
     	out.println("InetAddresses:");
     	
@@ -46,6 +76,12 @@ public class ListNIFs
     }
     
     
+    /**
+     * Displays MAC address in hex format, separated with ':'
+     * 
+     * @param netIf
+     * @throws SocketException
+     */
     static void displayMACAddress(NetworkInterface netIf) throws SocketException {
     	byte[] hardwareAddress = netIf.getHardwareAddress();
     	List<String> stringList = new ArrayList<>();
