0c560be45c1075acead86740b57efe7678e186a7
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-03-25 21:55:01 +1100

    Implemented separate socket for checksums

--- NetworkProgWeek4/src/echoserver/Client.java
+++ NetworkProgWeek4/src/echoserver/Client.java
@@ -6,14 +6,24 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.zip.CRC32;
+import java.util.zip.CheckedOutputStream;
 
 
 
 /**
- * Simple program that send lines of messages to a socket server<br/><br/>
- * On startup, you may provide these 2 optional arguments:<br/>
- * - <b>Host name</b>. Defaults to '127.0.0.1'<br/>
- * - <b>Port number</b>. Defaults to port 15376<br/>
+ * EchoServer client
+ * <p>This program sends lines of messages to the server and receives a 
+ * reply back. When server replies back an uppercase 'X' character, the 
+ * client quits.
+ * <p>This client also calculates and sends the resulting checksum to 
+ * a separate socket connection. 
+ * <p>You may provide the main(String[] args) will 3 command line 
+ * arguments.
+ * <p>1. Host address. Defaults to '127.0.0.1'
+ * <p> 2. Main port number to connect to. Defaults to port 15376.
+ * <p>3. Port number for sending checksum values. Defaults to port 
+ * 25376.
  */
 public class Client {
 	
@@ -24,16 +34,36 @@ public class Client {
 		
 		// get port and host from command line args
 		int port = getPortNumber(args);
+		int checksumPort = getPortNumberForChecksums(args);
 		String address = getHostAddress(args);
+
+		if (port == checksumPort) {
+			System.err.printf("\n******ERROR: Main port and port for checksums should NOT be the same******\n\n");
+			return;
+		}
+
 		
+		// main socket and readers
 		Socket socket = null;
+		CheckedOutputStream checkedOutputStream = null;
 		PrintWriter output = null;
 		BufferedReader input = null;
-		BufferedReader consoleInput = null;
+		BufferedReader console = null;
 		
+		// another socket to send checksums
+		Socket checksumSocket = null;
+		PrintWriter checksumWriter = null;
+
 		try {
-			// connect to server
+
+			/**
+			 * Connect to the server with 2 sockets
+			 * - main socket for sending messages
+			 * - another socket for sending checksums
+			 */
 			socket = new Socket(address, port);
+			checksumSocket = new Socket(address, checksumPort);
+
 			String localSocketAddress = socket.getLocalAddress().getHostAddress();
 			int localPortNumber = socket.getLocalPort();
 			System.out.printf("Client %s:%d successfully launched\n", localSocketAddress, localPortNumber);
@@ -41,25 +71,40 @@ public class Client {
 			
 			
 			// setup input and output streams
-			output = new PrintWriter(socket.getOutputStream(), true);
+			checkedOutputStream = new CheckedOutputStream(socket.getOutputStream(), new CRC32());
+			output = new PrintWriter(checkedOutputStream, true);
 			input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			consoleInput = new BufferedReader(new InputStreamReader(System.in));
-			
+			console = new BufferedReader(new InputStreamReader(System.in));
+			checksumWriter = new PrintWriter(checksumSocket.getOutputStream(), true);
 			
 			printInstructions();
 			
-			
-			// Read line from console. On enter pressed, this client sends the message to the server
-			// This client expects replies from server. 
-			// When this client receives an 'X' character, the program quits
+
+			/**
+			 * Read a line from console. When 'Enter' is pressed, 2 things happen:
+			 * - sends the message line to the server via the main socket (default port: 15376).
+			 * - calculates the checksum, then sends the result to socket assigned to checksums (default port: 25376)
+			 * 
+			 * This also expects an immediate reply from the server. Server replies are printed.
+			 * If the reply is an single uppercase 'X' this client program quits.
+			 */
 			String line;
 			System.out.print("Input: ");
-			while ((line = consoleInput.readLine()) != null) {
+			while ((line = console.readLine()) != null) {
+				// send the message to the main socket
 				output.println(line);
-				
+
+				// calculates the checksum, 
+				// send the result to assigned socket for checksums
+				long checksum = checkedOutputStream.getChecksum().getValue();
+				checksumWriter.println(checksum);
+				System.out.println("Checksum: " + Long.toString(checksum));
+
+				// Print server replies
 				String reply = input.readLine();
 				System.out.println("Reply: " + reply);
 				
+				// quit if reply is 'X'
 				if (reply.equals("X")) {
 					break;
 				}
@@ -75,18 +120,21 @@ public class Client {
 			System.err.println("ERROR: Unknown host " + address);
 			unknownHostEx.printStackTrace();
 		}
-		// Some errors like network interruption, security problems, etc
+		// Some errors like network interruption, security problems, server not found, etc
 		catch (IOException ioEx) {
-			System.err.println("ERROR: Sorry! Server hasn't started yet or something went wrong");
+			System.err.println("ERROR: Sorry! Something went wrong. " + ioEx.getMessage());
 			ioEx.printStackTrace();
 		}
 		// close connection gracefully, 
-		// socket connection opened first, so close it last.
+		// main socket opened first, so close it last.
 		finally {
 			try {
-				if (consoleInput != null) consoleInput.close();
+				if (checksumWriter != null) checksumWriter.close();
+				if (checksumSocket != null) checksumSocket.close();
+				if (console != null) console.close();
 				if (input != null) input.close();
 				if (output != null) output.close();
+				if (checkedOutputStream != null) checkedOutputStream.close();
 				if (socket != null) socket.close();
 			} catch (IOException e) {
 				System.err.println("ERROR: Something went wrong while closing the connection");
@@ -97,8 +145,8 @@ public class Client {
 
 	
 	/**
-	 * Gets port number from command line arguments.<br/>
-	 * Port number must be the second command line argument
+	 * Gets the main port number from command line arguments.<br/>
+	 * The main port number must be the second command line argument
 	 * 
 	 * @param args
 	 * @return int
@@ -119,6 +167,33 @@ public class Client {
 		
 		return portNumber;
 	}
+
+
+	/**
+	 * Gets the 3rd argument from the main(String[] args).
+	 * <p>The 3rd argument will be used as port for sending checksums
+	 * 
+	 * @param String[] Command line arguments
+	 * @return int
+	 */
+	public static int getPortNumberForChecksums(String[] args) {
+		int portNumberForChecksums = Server.CHECKSUM_PORT_NUMBER;
+
+		if (args.length < 3) {
+			return portNumberForChecksums;
+		}
+
+		try {
+			portNumberForChecksums = Integer.parseInt(args[2]);
+		}
+		catch (NumberFormatException numFormatEx) {
+			System.err.printf("ERROR: %s is not a valid port number\n", args[2]);
+		}
+
+		return portNumberForChecksums;
+	}
+
+
 	
 	
 	/**
@@ -145,12 +220,12 @@ public class Client {
 	 */
 	public static void printInstructions() {
 		System.out.println();
-		System.out.println("+----------------------------------+");
-		System.out.println("| INSTRUCTIONS:                    |");
-		System.out.println("| - Type your input.               |");
-		System.out.println("| - Press enter to send            |");
-		System.out.println("| - Type 'X' then enter to exit    |");
-		System.out.println("+----------------------------------+");
+		System.out.println("+----------------------------------------+");
+		System.out.println("| INSTRUCTIONS:                          |");
+		System.out.println("| - Type your input.                     |");
+		System.out.println("| - Press enter to send                  |");
+		System.out.println("| - Type 'X' or 'x' then enter to exit   |");
+		System.out.println("+----------------------------------------+");
 		System.out.println();
 	}
 	
--- NetworkProgWeek4/src/echoserver/Server.java
+++ NetworkProgWeek4/src/echoserver/Server.java
@@ -9,31 +9,46 @@ import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
+import java.util.zip.CRC32;
+import java.util.zip.CheckedInputStream;
 
 
 /**
- * Simple server program that takes user input and throws 
- * it back to the client<br/><br/>
- * Launches a socket server at port 15376 by default.<br/>
- * Server will continue to listen unless the user 
- * stops the server manually or some error happens
+ * Simple server program that takes user input and throws it back 
+ * to the client. This server will continue to listen unless the 
+ * user stops the server manually or some error happens.
+ * <p>
+ * Hosts 2 socket connections:
+ * <p>
+ * 1. The first socket will read client's messages and reply back the 
+ * received messages as upper-case text. This socket runs on port 
+ * 15376 by default (can be overriden by supplying arguments to 
+ * main())
+ * <br/>
+ * 2. The other socket will get checksums sent by the client. The 
+ * server will tell if checksums sent by the client and calculated 
+ * from this server matches<p>
  */
 public class Server {
 	
-	
 	public static final int DEFAULT_PORT_NUMBER = 15376;
+	public static final int CHECKSUM_PORT_NUMBER = 25376;
+
 	public static final String LOG_FILE = "echoserver.log";
 	public static Logger logger;
 	
 	
 	/**
 	 * Set up logging. New logs are appended. 
-	 * If log file doesn't exist, it is automatically created
+	 * If log file doesn't exist, it is automatically created.
 	 */
 	static {
 		logger = Logger.getLogger(Server.class.getName());
 		
-		// Disable default console logging
+		/**
+		 * Logger prints the log to the console even if we add
+		 * new log handlers. We don't want that.
+		 */
 		logger.setUseParentHandlers(false);
 		
 		FileHandler fileHandler = null;
@@ -56,53 +71,93 @@ public class Server {
 		}
 	}
 	
-	
+	/**
+	 * Main method can take 2 optional arguments
+	 * <p>
+	 * 1. The main port number for receiving client 
+	 * messages. Defaults to port <b>15376</b>
+	 * <p>
+	 * 2. Port for receiving checksums calculated by the 
+	 * client. Defaults to port <b>25376</b>
+	 */
 	public static void main(String[] args) {
 		
 		// get the port from args. If args is empty, use port 15376
 		int port = getPortNumber(args);
+		int checksumPort = getPortNumberForChecksums(args);
+
+		if (port == checksumPort) {
+			System.err.printf("\n******ERROR: Main port and port for checksums should NOT be the same******\n\n");
+			return;
+		}
 		
+		// main socket
 		ServerSocket serverSocket = null;
 		Socket clientSocket = null;
-		PrintWriter out = null;
-		BufferedReader input = null;
+		CheckedInputStream checkedInputStream = null;
+		PrintWriter writer = null;
+		BufferedReader reader = null;
+
+		// Separate socket for reading checksum 
+		ServerSocket serverSocketForChecksums = null;
+		Socket socketForChecksums = null;
+		BufferedReader readerForChecksums = null;
+
 		
 		try {
-			// create server
+			// create 2 servers, the main one and for checksums
 			serverSocket = new ServerSocket(port);
+			serverSocketForChecksums = new ServerSocket(checksumPort);
+
 			waitingForConnection(port);
 			
 			
 			// listen for connections 24/7
 			do {
-				// accept client
+
+				// listen for main socket and socket for receiving checksums
 				clientSocket = serverSocket.accept();
+				socketForChecksums = serverSocketForChecksums.accept();
 				clientConnected(clientSocket);
+
 				
-				// accept input
-				out = new PrintWriter(clientSocket.getOutputStream(), true); // true: auto-flush
-				input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				// read responses
+				checkedInputStream = new CheckedInputStream(clientSocket.getInputStream(), new CRC32());
+				reader = new BufferedReader(new InputStreamReader(checkedInputStream));
+				writer = new PrintWriter(clientSocket.getOutputStream(), true); // true means keep accepting new lines
+
+				// read checksums
+				readerForChecksums = new BufferedReader(new InputStreamReader(socketForChecksums.getInputStream()));
+
 				
-				// Print client message, then reply back
+				// - Print client message, then reply back.
+				// - When client presses enter, checksums will also be sent 
+				//   to this server via a separate socket.
 				String line;
-				while ((line = input.readLine()) != null) {
-					
-					// log message
+				while ((line = reader.readLine()) != null) {
+
+					// print and log client messages
 					printClientMessage(line);
 					log(line);
+
+					// read and evaluate checksum whether they match
+					String checksumFromClient = readerForChecksums.readLine();
+					long calculatedChecksum = checkedInputStream.getChecksum().getValue();
+					boolean isChecksumsMatch = checksumFromClient.trim().equals(Long.toString(calculatedChecksum));
+					System.out.println("---calculated checksum: " + calculatedChecksum);
+					System.out.println("---checksum from client: " + checksumFromClient);
+					System.out.println("---checksums match? " + (isChecksumsMatch ? "yes" : "NO"));
 					
 					// reply back the client's message as upper-case letters
 					String response = line.toUpperCase();
-					out.println(response);
+					writer.println(response);
 				}
 				
 				clientDisconnected();
 				waitingForConnection(port);
 				
 			} while (true);
-			
-			
-			
+
 		} 
 		// Invalid port number
 		catch (IllegalArgumentException argEx) {
@@ -113,10 +168,15 @@ public class Server {
 			System.err.println("Sorry! Something went wrong.");
 			ioEx.printStackTrace();
 		} 
+		// close the streams and sockets
 		finally {
 			try {
-				if (input != null) input.close();
-				if (out != null) out.close();
+				if (readerForChecksums != null) readerForChecksums.close();
+				if (socketForChecksums != null) socketForChecksums.close();
+				if (serverSocketForChecksums != null) serverSocketForChecksums.close();
+				if (reader != null) reader.close();
+				if (writer != null) writer.close();
+				if (checkedInputStream != null) checkedInputStream.close(); 
 				if (clientSocket != null) clientSocket.close();
 				if (serverSocket != null) serverSocket.close();
 			} catch (IOException e) {
@@ -126,21 +186,29 @@ public class Server {
 	}
 	
 	
+	/**
+	 * Simple callback for printing client's messages
+	 * @param String Client message
+	 */
 	public static void printClientMessage(String line) {
 		System.out.println("Client: " + line);
 	}
 
 
 	/**
-	 * Get the port number provided by command line arguments
+	 * Get the first port number provided by command line arguments 
+	 * (main(String[] args))
+	 * <p>
+	 * The first port number will be used to receive client messages
 	 * 
-	 * @param args
+	 * @param args If there are no args provided (ie. empty array), 
+	 * 			   it will use the default port 15376
 	 * @return int
 	 */
 	public static int getPortNumber(String[] args) {
 		int portNumber = DEFAULT_PORT_NUMBER;
 		
-		if (args.length == 0) {
+		if (args.length < 1) {
 			return portNumber;
 		}
 		
@@ -153,10 +221,39 @@ public class Server {
 		
 		return portNumber;
 	}
+
+	
+	/**
+	 * Get the 2 port number from command line arguments (main(String[] args))
+	 * <p>
+	 * The second port number will be used to get checksums calculated by the 
+	 * client
+	 * 
+	 * @param args If number of args is less than 2, it will use port 25376
+	 * @return int
+	 */
+	public static int getPortNumberForChecksums(String[] args) {
+		int portNumberForChecksums = CHECKSUM_PORT_NUMBER;
+
+		if (args.length < 2) {
+			return portNumberForChecksums;
+		}
+
+		try {
+			portNumberForChecksums = Integer.parseInt(args[1]);
+		}
+		catch (NumberFormatException numFormatEx) {
+			System.err.printf("ERROR: %s is not a valid port number\n", args[1]);
+		}
+
+		return portNumberForChecksums;
+	}
 	
 	
 	/**
 	 * Simple callback saying the client is connected
+	 * 
+	 * @param Socket
 	 */
 	public static void clientConnected(Socket socket) {
 		String peerHostAddress = "[unknown peer address]";
