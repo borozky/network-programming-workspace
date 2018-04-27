8010c204d9395c7616a0a28ef6bef5b61d5f4499
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-04-27 19:47:25 +1000

    Initial commit for week9 lab code

---
+++ NetworkProgWeek9/src/jarurlconnection/Program.java
@@ -0,0 +1,64 @@
+package jarurlconnection;
+
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+public class Program {
+	
+	public static final String JAR_URL_ADDRESS = "http://m1-c45n1.csit.rmit.edu.au//~Course/HelloWorld.jar";
+
+	public static void main(String[] args) {
+		
+		URL jarUrl = null;
+		JarURLConnection jarURLConnection = null;
+		JarFile jarFile = null;
+		
+		try {
+			jarUrl = new URL(JAR_URL_ADDRESS);
+			jarURLConnection = (JarURLConnection) jarUrl.openConnection();
+			
+			// content type
+			String contentType = jarURLConnection.getContentType();
+			System.out.printf("CONTENT-TYPE: %s\n" + contentType);
+			
+			// content length
+			int contentLength = jarURLConnection.getContentLength();
+			System.out.printf("CONTENT-LENGTH: %d\n" + contentLength);
+			
+			// get entries
+			jarFile = jarURLConnection.getJarFile();
+			Enumeration<JarEntry> jarEntries = jarFile.entries();
+			
+			// iterate each entry
+			while (jarEntries.hasMoreElements()) {
+				JarEntry entry = jarEntries.nextElement();
+				String entryName = entry.getName();
+				long entrySize = entry.getSize();
+				System.out.printf("%s (%l bytes)\n", entryName, entrySize);
+			}
+		}
+		catch (UnknownHostException e) {
+			System.err.println("JAR cannot be found. " + e.getMessage());
+		}
+		catch (MalformedURLException e) {
+			System.err.println("Invalid url. " + e.getMessage());
+		}
+		catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+		finally {
+			
+			// nothing to close here
+			
+		}
+		
+
+	}
+
+}
